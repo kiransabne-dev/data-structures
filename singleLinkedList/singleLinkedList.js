956227bb70700c46e463d326e159b07645aac7f1
kiran <kiransabne18@gmail.com>
2020-01-04 12:54:59 +0530

    adding reverse function for singleLinkedList

--- singleLinkedList/singleLinkedList.js
+++ singleLinkedList/singleLinkedList.js
@@ -2,30 +2,26 @@ class Node {
   constructor(value){
     this.value = value
     this.next = null
-	
   }
 }
 
-// 1 -> 2 -> 3 -> 5 -> 6
-class SingleList {
+class SingleLinkedList {
   constructor(){
     this.first = null
     this.last = null
-	this.length = 0
+    this.size = 0
   }
 
   addFirst(value){
     const node = new Node(value)
     node.next = this.first
     if(this.first){
-      //if list is not empty
       this.first = node
-	  this.length++
+      this.size++
     } else {
-      //if list is empty
       this.first = node
       this.last = node
-	  this.length++
+      this.size++
     }
   }
 
@@ -33,34 +29,31 @@ class SingleList {
     const first = this.first
     if(first){
       this.first = first.next
-	  this.length--
+      this.size--
       return first.value
-    } else {
+    }else {
       this.first = null
       this.last = null
-	  this.length--
+      this.size--
     }
   }
 
   addLast(value){
     const node = new Node(value)
     if(this.first){
-      //list is not empty
-      const current = this.first
       this.last.next = node
       this.last = node
-	  this.length++
+      this.size++
     } else {
-      //if is empty
       this.first = node
       this.last = node
-	  this.length++
+      this.size++
     }
   }
 
   removeLast(){
     let current = this.first
-    let target 
+    let target
     if(current && current.next){
       while(current && current.next && current.next.next){
         current = current.next
@@ -68,72 +61,14 @@ class SingleList {
       this.last = current
       target = current.next
       current.next = null
-	  this.length--
+      this.size--
     } else {
       this.first = null
       this.last = null
-      target = current
-	  this.length--
-    }
-    if(target){
-      return target.value
-    }
-  }
-// 1 -> 2 -> 3 -> 5 -> 6
-  addAt(nth, value){
-    const node = new Node(value)
-    if(nth === 0){
-      return this.addFirst(value)
-
-    }
-    for(let current = this.first, index = 0; current; current = current.next, index++){
-      if(index === nth){
-        if(!current.next){
-          return this.addLast(value)
-        }
-        node.next = current.next
-        current.next = node
-		this.length++
-      }
+      this.size--
     }
   }
 
-  removeAt(nth){
-    if(nth === 0){
-      return this.removeFirst()
-    }
-	else if(nth === (this.length - 1)){
-		return this.removeLast()
-	} 
-	else {
-		const previousNodeOfNodeToRemove = this.get(nth - 1)
-		
-		let nodeToRemove = previousNodeOfNodeToRemove.next
-		previousNodeOfNodeToRemove.next = nodeToRemove.next
-		this.length--
-		return nodeToRemove
-	}
-	
- 
-	
-  }
-	get(index) {
-    if (index < 0 || index >= this.length) {
-      return null;
-    } else {
-      let count = 0;
-      let currentNode = this.first;
-
-      while (count < index) {
-        currentNode = currentNode.next;
-        count += 1;
-      }
-
-      return currentNode;
-    }
-  }
-
-	
   print(){
     let current = this.first
     let arr = []
@@ -144,47 +79,80 @@ class SingleList {
     return arr
   }
 
-  contains(value) {
-      for (let current = this.first, index = 0; current; index++, current = current.next) {
-        if (current.value === value) {
-          return index;
+  get(position){
+    if(position < 0 || position >= this.size){
+      return null
+    }
+    let current = this.first
+    let counter = 0
+    while(counter < position){
+      current = current.next
+      counter++
+    }
+    return current
+  }
+
+  //1-> 2-> 3-> 4-> 5-> null 
+  addAt(position, value){
+    if(position === 0){
+      return this.addFirst(value)
+    } 
+    for(let current = this.first, index = 0; current; current = current.next, index++){
+      if(index === position){
+        if(!current.next){
+          return this.addLast(value)
         }
+        const node = new Node(value)
+        node.next = current.next
+        current.next = node
+        this.size++
       }
     }
+  }
 
+  removeAt(position){
+    if(position === 0){
+      return this.removeFirst()
+    }
+    else if(position ===(this.size - 1)){
+      return this.removeLast()
+    } else {
+      let previousNodeOfTheNodeToBeRemoved = this.get(position - 1)
+      let nodeToBeRemoved = previousNodeOfTheNodeToBeRemoved.next
+      //set
+      previousNodeOfTheNodeToBeRemoved.next = nodeToBeRemoved.next
+      this.size--
+      return nodeToBeRemoved
+    }
+  }
 
+  reverse(){
+    let node = this.first
+    let previous = null, temp;
+    while(node){
+      temp = node.next
+      node.next = previous
+      previous = node
+      node = temp
+    }
+    return previous
+  }
 
 }
 
-const list = new SingleList()
-//console.log(list)
-
-list.addFirst('first')
-list.addFirst('Second')
-list.addFirst('Third')
-//console.log(list)
-//console.log('list =>', list.print())
-
-list.removeFirst()
-//console.log('after removing first =>', list)
-
-
-list.addLast('fourth')
-list.addLast('fifth')
-//console.log('list =>', list.print())
-//list.print()
-list.removeLast()
-//console.log('list =>', list.print())
-list.addAt(1, 'added between')
-console.log('list =>', list.print())
-console.log(list.contains('added between'))
-//console.log('remove at => ', list.removeAt(1))
-console.log('get => ', list.get(1))
-console.log('removeAt => ', list.removeAt(1))
-console.log('list =>', list.print())
-
-//console.log(list)
-//console.log('list =>', list)
-// console.log('after adding to last in list => ', list)
-// list.print()
-
+const linkedList = new SingleLinkedList()
+linkedList.addFirst('first')
+linkedList.addFirst('second')
+linkedList.addLast('third')
+linkedList.addLast('fourth')
+console.log(linkedList.print())
+linkedList.removeFirst()
+console.log('remove First => ', linkedList.print())
+linkedList.removeLast()
+console.log('remove Last => ', linkedList.print())
+console.log(linkedList.get(0))
+linkedList.addAt(1, 'added at')
+console.log('add at => ', linkedList.print())
+linkedList.removeAt(1)
+console.log('remove at => ', linkedList.print())
+console.log('reverse =>', linkedList.reverse())
